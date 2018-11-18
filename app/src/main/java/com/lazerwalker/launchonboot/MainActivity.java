b4ee6dba76d3d394928e6c2ad2302f5a8e0947e3
Mike Lazer-Walker <michael@lazerwalker.com>
2018-11-18 21:08:21 +0100

    Open Settings.app when the Menu button is pressed
    For testing purposes. With our stick, we need access to settings.app to enable USB debugging, so this ensures uor testing won't brick our device

--- app/src/main/java/com/lazerwalker/launchonboot/MainActivity.java
+++ app/src/main/java/com/lazerwalker/launchonboot/MainActivity.java
@@ -1,7 +1,9 @@
 package com.lazerwalker.launchonboot;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        switch(keyCode){
+            case KeyEvent.KEYCODE_MENU:
+                Intent settings = new Intent(android.provider.Settings.ACTION_SETTINGS);
+                settings.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(settings);
+        }
+        return super.onKeyDown(keyCode, event);
     }
 }
